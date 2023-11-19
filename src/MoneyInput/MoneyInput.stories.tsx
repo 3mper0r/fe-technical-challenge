242adad225fc2c94578859edc3a421aa5ce34ea7
Elidon Morina <83674499+3mper0r@users.noreply.github.com>
2023-11-19 10:58:17 +0100

    Build UI

--- src/MoneyInput/MoneyInput.stories.tsx
+++ src/MoneyInput/MoneyInput.stories.tsx
@@ -7,7 +7,7 @@ export default {
   component: MoneyInput,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
-    layout: 'centered',
+    // layout: 'centered', //<-- commented this out so I can use grid to make it look identical to the Interface as challenge requires
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
   tags: ['autodocs'],
--- src/MoneyInput/MoneyInput.tsx
+++ src/MoneyInput/MoneyInput.tsx
@@ -1,5 +1,51 @@
 import _styles from './MoneyInput.module.css'
 
 export default function MoneyInput() {
-  return null
+  return (
+  <main>
+    <section className={_styles.wrapper}>
+      <label htmlFor="default" >
+        <span className={_styles.labelLength}>State: Default</span>
+        <input type="text" className={`${_styles.OnFocus} ${_styles.margin}`} placeholder='Text'/>
+      </label>   
+      <label htmlFor="hover">
+        <span className={_styles.labelLength}>State: Hover</span>
+        <input type="text" className={`${_styles.OnHover} ${_styles.margin}`} placeholder='Text'/>
+      </label>
+      <label htmlFor="focus">
+        <span className={_styles.labelLength}>State: &nbsp; Focus</span>
+        <input type="text" className={`${_styles.focus} ${_styles.margin}`} placeholder='Text'/>
+      </label>
+      <label htmlFor="error">
+        <span className={_styles.labelLength}>State: Error</span>
+        <input type="text" className={`${_styles.error} ${_styles.margin}`} placeholder='Text'/>
+      </label>
+      <label htmlFor="error-hover">
+        <span className={_styles.labelLength}>State: Error +&nbsp;Hover</span>
+        <input type="text" className={`${_styles.error} ${_styles.margin}`} placeholder='Text'/>
+      </label>
+      <label htmlFor="error-focus">
+        <span className={_styles.labelLength}>State: Error +&nbsp;Focus</span>
+        <input type="text" className={`${_styles.errorAndFocused} ${_styles.margin}`} placeholder='Text' />
+      </label>
+      <label htmlFor="disabled">
+        <span className={_styles.labelLength}>State: Disabled</span>
+        <input type="text" disabled className={_styles.margin} placeholder='Text'/>
+      </label>
+      <label htmlFor="disabled-focus">
+        <span className={_styles.labelLength}>State: Disabled +&nbsp;Focus</span>
+        <input type="text" readOnly tabIndex={0} aria-disabled={true} className={`${_styles.OnFocus} ${_styles.margin}`} placeholder='Text' />
+      </label>
+    </section>
+    <section>
+      <label htmlFor="default-label" className={_styles.defaultLabel}>
+        <span className={_styles.specialLabel}><b>Label*</b></span>
+        <input type="text" className={_styles.OnFocus} placeholder='Default' />
+      </label>
+    </section>
+  </main>
+  )
 }
+
+
+// {`${styles.description} ${styles.yellow}`}
