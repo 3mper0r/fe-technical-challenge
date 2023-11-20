4ebbd665999d22337e10cff95b34177aca4f5058
Elidon Morina <83674499+3mper0r@users.noreply.github.com>
2023-11-20 13:48:30 +0100

    feature: added the currency to be shown on label only when we provide a value externally

--- src/MoneyInput/MoneyInput.tsx
+++ src/MoneyInput/MoneyInput.tsx
@@ -3,6 +3,7 @@ import _styles from './MoneyInput.module.css'
 
 interface Props {
   value?: number;
+  locale?: string
 }
 
 // - initial value is always 0, then when we type in input, input changes onChange and onBlur (logges in console)
@@ -10,9 +11,18 @@ interface Props {
 // - to test onChange and onBlur the value prop must be removed
 // - I have used label* input to show results on change
 
-const MoneyInput = ({value=1000}: Props) => {
+// ***If we have an initial value on render only then locale works, to check other functionality we have to remove these ***
+// locale is either us for dollars or de for euro
+
+const MoneyInput = ({value, locale='us'}: Props) => {
   
   const [euro, setEuro] = useState<number | string>(0)
+  
+  
+  const formatter = new Intl.NumberFormat(`en-${locale}`, {
+    style: 'currency',
+    currency: locale === 'de' ? 'EUR' : 'USD',
+  });
 
   useEffect(() => {
     if (value !== undefined && !isNaN(value)) {
@@ -81,7 +91,7 @@ const MoneyInput = ({value=1000}: Props) => {
     <section>
       <label htmlFor="default-label" className={_styles.defaultLabel}>
         <span className={_styles.specialLabel}>Label*</span>
-        <input type="text" value={euro} onChange={handleChange} onBlur={handleBlur} className={_styles.OnFocus} placeholder='Default' id='default-label'/>
+        <input type="text" value={formatter.format(Number(euro))} onChange={handleChange} onBlur={handleBlur} className={_styles.OnFocus} placeholder='Default' id='default-label'/>
       </label>
     </section>
   </main>
